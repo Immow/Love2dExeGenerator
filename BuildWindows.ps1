7446c2065d4bbb9cd05f87173f5b1ea815670f36
Immow <k.schippers@gmail.com>
2024-03-16 17:34:52 +0100

    removed obsolete code that did nothing

--- BuildWindows.ps1
+++ BuildWindows.ps1
@@ -2,14 +2,12 @@ $path1 = "C:\Dev\love-windows" # Output folder
 $path2 = "C:\Program Files\LOVE" # Löve2d installation path
 $path3 = "D:\Documents\Programming\Lua\Test\FFI" # Path of your game project
 
-# Check if game.exe exists in the output folder and remove it if it does
-if (Test-Path "$path1\game.exe") {
-	Remove-Item "$path1\game.exe"
-}
+# Remove game.zip, game.exe, and the bin folder if they exist
+Remove-Item "$path1\game.zip", "$path1\game.exe", "$path1\bin" -ErrorAction SilentlyContinue -Recurse
 
-# Remove all files in the bin folder of the output folder
-if (Test-Path "$path1\bin") {
-	Remove-Item "$path1\bin\*.*"
+# Check if the bin folder exists, if not, create it
+if (-not (Test-Path "$path1\bin")) {
+	New-Item -Path "$path1\bin" -ItemType Directory -Force
 }
 
 try {
@@ -19,9 +17,6 @@ try {
 	# Concatenate love.exe and the game.zip into game.exe
 	Get-Content -raw "$path2\love.exe", "$path1\game.zip" -Encoding Byte |
 	Set-Content -Encoding Byte -NoNewline "$path1\game.exe"
-
-	# Remove the temporary game.zip
-	Remove-Item "$path1\game.zip"
 
 	# Copy necessary DLLs from Löve2d installation folder to the output folder
 	Copy-Item -Path "$path2\*" -Include "*.dll" -Destination $path1 -Force
