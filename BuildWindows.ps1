016dbafb511de4a8853d0432da4e60faac4eec93
Immow <k.schippers@gmail.com>
2024-03-16 02:07:57 +0100

    Added a bin folder so we can use libs like dll files.

--- BuildWindows.ps1
+++ BuildWindows.ps1
@@ -1,15 +1,34 @@
-$path1 = "C:\Dev\love-windows" #Output folder
-$path2 = "C:\Program Files\LOVE" #Löve2d installtion path
-$path3 = "D:\temp\*" #Path of your game project
+$path1 = "C:\Dev\love-windows" # Output folder
+$path2 = "C:\Program Files\LOVE" # Löve2d installation path
+$path3 = "D:\Documents\Programming\Lua\Test\FFI" # Path of your game project
+
+# Check if game.exe exists in the output folder and remove it if it does
 if (Test-Path "$path1\game.exe") {
-    Remove-Item "$path1\game.exe" 
+	Remove-Item "$path1\game.exe"
 }
+
+# Remove all files in the bin folder of the output folder
+if (Test-Path "$path1\bin") {
+	Remove-Item "$path1\bin\*.*"
+}
+
 try {
-    Compress-Archive -Path $path3 -DestinationPath "$path1\game.zip"
-    Get-Content -raw "$path2\love.exe","$path1\game.zip" -Encoding Byte | Set-Content -Encoding Byte -NoNewline "$path1\game.exe"
-    # Remove-Item "$path1\game.zip"
-    Copy-Item -Path $path2\* -Include "*.dll" -Destination $path1
+	# Compress the entire game project folder into a zip file
+	Compress-Archive -Path $path3\* -DestinationPath "$path1\game.zip" -Force
+
+	# Concatenate love.exe and the game.zip into game.exe
+	Get-Content -raw "$path2\love.exe", "$path1\game.zip" -Encoding Byte |
+	Set-Content -Encoding Byte -NoNewline "$path1\game.exe"
+
+	# Remove the temporary game.zip
+	Remove-Item "$path1\game.zip"
+
+	# Copy necessary DLLs from Löve2d installation folder to the output folder
+	Copy-Item -Path "$path2\*" -Include "*.dll" -Destination $path1 -Force
+
+	# Copy necessary DLLs from the game project's bin folder to the output folder's bin folder
+	Copy-Item -Path "$path3\bin\*" -Include "*.dll" -Destination "$path1\bin" -Force
 }
 catch {
-    throw $_.Exception.Message
+	Write-Error "Error occurred: $_"
 }
